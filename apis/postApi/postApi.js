2672dda9f7ab697a56de559981d5a1f16355c4f5
simanto97 <saimumsimanto97@gmail.com>
2024-03-14 11:20:40 +0600

    query search added

--- apis/postApi/postApi.js
+++ apis/postApi/postApi.js
@@ -3,8 +3,18 @@ const express = require("express");
 const postApi = (postCollection) => {
   const postRouter = express.Router();
 
+  // query search for posts
   postRouter.get("/", async (req, res) => {
-    const result = await postCollection.find().toArray();
+    let query = {};
+    const category = req.query.category;
+    const subCategory = req.query.subCategory;
+    if (req.query.category) {
+      query.category = category;
+    }
+    if (req.query.subCategory) {
+      query.subCategory = subCategory;
+    }
+    const result = await postCollection.find(query).toArray();
 
     if (result.length === 0) {
       return res.status(404).json({ message: "No data available" });
